f3239cc0ed8f7e3a68169cfc70c89572542a6a41
adityranjan1011 <adityamnnit009@gmail.com>
2020-07-26 17:17:51 +0530

    Updates

---
+++ precache-manifest.fbdc5c9fedd5746583d6ad78de7d17e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbdfbddae08b6128053ef932ae2485a",
+    "url": "/UserList/index.html"
+  },
+  {
+    "revision": "bfb6fefc9a8a46f366ff",
+    "url": "/UserList/static/css/main.d75f9b53.chunk.css"
+  },
+  {
+    "revision": "7f4718de293f90c2ab1e",
+    "url": "/UserList/static/js/2.d5999965.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/UserList/static/js/2.d5999965.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfb6fefc9a8a46f366ff",
+    "url": "/UserList/static/js/main.b891299e.chunk.js"
+  },
+  {
+    "revision": "0f2eb2599ffb6a240b44",
+    "url": "/UserList/static/js/runtime-main.08c3ff38.js"
+  }
+]);
